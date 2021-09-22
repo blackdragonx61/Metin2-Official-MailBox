08b4cd8a6ff6663d0e7e6f37bb178003c6d71201
blackdragonx61 <33325253+blackdragonx61@users.noreply.github.com>
2021-09-22 18:53:00 +0300

    Update PythonApplicationModule.cpp

--- 1.Svn/Client/UserInterface/PythonApplicationModule.cpp
+++ 1.Svn/Client/UserInterface/PythonApplicationModule.cpp
@@ -8,12 +8,13 @@
 ///Add
 #if defined(__BL_MAILBOX__)
 	PyModule_AddIntConstant(poModule, "BL_MAILBOX", true);
+#else
+	PyModule_AddIntConstant(poModule, "BL_MAILBOX", false);
+#endif
+
 #ifndef ENABLE_CHEQUE_SYSTEM
 	PyModule_AddIntConstant(poModule, "ENABLE_CHEQUE_SYSTEM", 0);
 #endif
 #if !defined(__BL_TRANSMUTATION__)
 	PyModule_AddIntConstant(poModule, "BL_TRANSMUTATION_SYSTEM", false);
-#endif
-#else
-	PyModule_AddIntConstant(poModule, "BL_MAILBOX", false);
 #endif
